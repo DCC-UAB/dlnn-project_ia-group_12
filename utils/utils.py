0d51b075811cb08346b0dde80b38e94572c91ceb
RoysGC <1633372@uab.cat>
2023-05-29 16:25:45 +0000

    inici comentari

--- utils/utils.py
+++ utils/utils.py
@@ -575,3 +575,93 @@ def initialize_resnet(num_classes):
     input_size = 224
         
     return model, input_size
+
+
+def initialize_mobilnet(num_classes):
+    # Resnet18 
+    model = models.mobilenet_v2(pretrained = True)
+    
+    # Modify the classifier to output a single value (0 or 1)
+    model.classifier = nn.Sequential(
+        nn.Dropout(0.2),
+        nn.Linear(1280, num_classes)
+    )
+    
+    input_size = 224
+        
+    return model, input_size
+
+
+
+def train_modelMOBILNET(device, model, dataloaders, criterion, optimizer, num_epochs=25):
+    since = time.time()
+
+    acc_history = {"train": [], "val": []}
+    losses = {"train": [], "val": []}
+
+    # we will keep a copy of the best weights so far according to validation accuracy
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    losses[phase].append(loss.item())
+
+                    preds = torch.argmax(outputs, dim=1)
+                    
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data).item() #running_corrects += torch.sum(preds == labels.data).cpu().numpy()
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            
+            acc_history[phase].append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, acc_history, losses
